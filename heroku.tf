0606e92e1404437f788983616f3ee9ac9b01af67
dwiechert <dan.wiechert@gmail.com>
2020-06-24 19:46:09 -0500

    Deploy works.

--- heroku.tf
+++ heroku.tf
@@ -1,14 +1,14 @@
 # Example copied from - https://www.terraform.io/docs/github-actions/setup-terraform.html
 
-//terraform {
-// backend "remote" {
-//   organization = "cicd-series"
-//
-//   workspaces {
-//     name = "heroku-prod"
-//   }
-// }
-//}
+terraform {
+ backend "remote" {
+   organization = "cicd-series"
+
+   workspaces {
+     name = "heroku-prod"
+   }
+ }
+}
 
 provider "heroku" {
   version = "~> 2.0"
